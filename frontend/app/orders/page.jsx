b49604ce9d2435023354bb9a414bb079f367d37d
mbadiao <diaombaye832@gmail.com>
2024-07-13 16:18:33 +0000

    empty order list 3

--- frontend/app/orders/page.jsx
+++ frontend/app/orders/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState, useMemo, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
